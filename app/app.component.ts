04b653b5eda72885eaad5994e27969bdb4046b54
MicuAlexandruVlad <micualexandru131@yahoo.com>
2018-02-21 12:18:16 +0200

    Add files via upload

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,9 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `<div><h1>{{pageTitle}}</h1>
-  <div>My First Component</div></div>`
+             <app-products></app-products></div>`
 })
 
 export class AppComponent {
-  pageTitle = 'Product Management';
+ pageTitle = 'Product Management';
 }
